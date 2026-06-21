46660318a156fa74de0ea8653c21f49674ab55a6
GisDuck <sigaj04@mail.ru>
2026-06-21 16:21:30 +0300

    Испарвили шапку на пк через функцию

--- frontend/src/components/StoreHeader/StoreHeader.tsx
+++ frontend/src/components/StoreHeader/StoreHeader.tsx
@@ -1,8 +1,13 @@
+import { isTelegramDesktop } from "../../shared/telegram";
 import "./StoreHeader.css";
 
 export function StoreHeader() {
+  const headerClassName = isTelegramDesktop()
+    ? "store-header store-header--desktop"
+    : "store-header";
+
   return (
-    <header className="store-header">
+    <header className={headerClassName}>
       <span className="store-header__logo-frame">
         <img
           className="store-header__logo"
